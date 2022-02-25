53bef960724783314530f4c31c58d60ab07abdc4
Uítalo <uitalo.urss@gmail.com>
2022-02-24 23:22:19 -0300

    iniciando o modal de nova transação

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { GlobalStyle } from "./styles/global"
+import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
 
 export function App() {
+  const [isTransactionModalOpen, setIstransactionModalOpen] = useState(false);
+
+  function handleOpenTransactionModal() {
+    setIstransactionModalOpen(true);
+  }
+
+  function handleCloseTransactionModal(){
+    setIstransactionModalOpen(false);
+  }
+
   return (
     <>
-      <GlobalStyle />
-      <Header />
+      <Header onOpenTransactionModal={handleOpenTransactionModal}/>
       <Dashboard />
+      <Modal 
+      isOpen={isTransactionModalOpen}
+      onRequestClose={handleCloseTransactionModal}>
+        <h2>Desgraça!!</h2>
+      </Modal>
+      <GlobalStyle />
     </>
   );
 }
