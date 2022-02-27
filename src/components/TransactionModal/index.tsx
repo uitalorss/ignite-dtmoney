0f16a7e85736ae758f7188bbae99ec67bdfd204e
Uítalo <uitalo.urss@gmail.com>
2022-02-27 01:45:10 -0300

    anotações no código (anoto mesmo)

--- src/components/TransactionModal/index.tsx
+++ src/components/TransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import Modal from 'react-modal';
 import { RadioBox, Container, TransactionTypeContainer, ButtonModal } from './styles';
-import  closeImg  from '../../assets/close.svg';
+import closeImg from '../../assets/close.svg';
 import expenseImg from '../../assets/expense.svg';
 import incomeImg from '../../assets/income.svg';
 import { useState } from 'react';
@@ -10,52 +10,52 @@ interface TransactionModalProps {
   onRequestClose: () => void;
 }
 
-export function TransactionModal({isOpen, onRequestClose}: TransactionModalProps){
-  const [transactionType, setTransactionType] = useState('income');
+export function TransactionModal({ isOpen, onRequestClose }: TransactionModalProps) {
+  const [transactionType, setTransactionType] = useState(''); // criando o estado para guardar o valor da transação
 
   return (
-    <Modal 
-    isOpen={isOpen} //argumento que verifica o estado do modal
-    onRequestClose={onRequestClose} //Argumento que recebe como parâmetro a função que fecha o modal
-    overlayClassName="react-modal-overlay"
-    className='react-modal-content'
+    <Modal
+      isOpen={isOpen} //argumento que verifica o estado do modal
+      onRequestClose={onRequestClose} //Argumento que recebe como parâmetro a função que fecha o modal
+      overlayClassName="react-modal-overlay"
+      className='react-modal-content'
     >
       <button type="button" onClick={onRequestClose} className='react-modal-close'>
         <img src={closeImg} alt="icone-fechar-modal" />
       </button>
       <Container>
         <h2>Cadastrar Transação</h2>
-        <input 
-          type="text" 
-          placeholder='Título'  
+        <input
+          type="text"
+          placeholder='Título'
         />
-        <input 
-          type="number" 
-          placeholder='Valor'  
+        <input
+          type="number"
+          placeholder='Valor'
         />
         <TransactionTypeContainer>
           <RadioBox
             type='button'
-            onClick={() => setTransactionType('income')}
-            isActive={transactionType === 'income'}
+            onClick={() => setTransactionType('income')} //atribuindo o valor referido para a transição
+            isActive={transactionType === 'income'} //Essa propriedade criada retorna um boolean verificando se o atributo transactionType é igual ao valor desejado. 
           >
             <img src={incomeImg} alt="icone-entrada" />
             <span>Entrada</span>
           </RadioBox>
           <RadioBox
             type='button'
-            onClick={() => setTransactionType('expense')}
-            isActive={transactionType === 'expense'}
+            onClick={() => setTransactionType('expense')} //atribuindo o valor referido para a transição
+            isActive={transactionType === 'expense'} //Essa propriedade criada retorna um boolean verificando se o atributo transactionType é igual ao valor desejado. 
           >
             <img src={expenseImg} alt="icone-saída" />
             <span>Saída</span>
           </RadioBox>
         </TransactionTypeContainer>
-        <input 
-          type="text" 
-          placeholder='Categoria'  
+        <input
+          type="text"
+          placeholder='Categoria'
         />
-        <ButtonModal 
+        <ButtonModal
           type="submit"
         >
           Cadastrar
--- src/components/TransactionModal/styles.ts
+++ src/components/TransactionModal/styles.ts
@@ -49,15 +49,16 @@ export const TransactionTypeContainer = styled.div`
     margin-left: 0.5rem;
   }
 `
-
+//Interface criada para receber a propriedade que verificará se o botão está ativo ou não.
 interface RadioBoxProps {
   isActive: boolean;
 }
 
-export const RadioBox = styled.button<RadioBoxProps>`
+// Botões do TransactionTypeContainer foram extraídos para virar outro componente, afim de receber props.
+export const RadioBox = styled.button<RadioBoxProps>` //no TypeScript é necessário mudar o generics para habilitar a interface criada.
     height: 3rem;
     margin: 1rem 0;
-    background: ${(props) => { return props.isActive ? '#d7d7d4' : ''}};
+    background: ${(props) => { return props.isActive ? '#d7d7d4' : ''}}; //Fazendo a mágica da troca de cores do botão acontecer.
 
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
